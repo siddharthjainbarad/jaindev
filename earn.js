89aa9299072bc7ff0c8937cfe5ef7523e6ef7d96
siddharthisenxt <siddharthisnext@gmail.com>
2017-01-09 13:12:34 +0530

    Add files via upload

---
+++ earn.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+$('#1').attr('href','http://the-pocket-money.com/pm/edcb4e14db3446d4f2548b33c6a85f31').text("Pocket Money (Best)");
+$('#2').attr('href','http://www.freeb.in/FreeB/ws/invite/W_6ET30637').text("FreeB (Excellent)");
+$('#3').attr('href','https://www.fokat.co.in/fkt/ws/invite/F1DW7N635').text("Fokat Money (Excellent)");
+$('#4').attr('href','http://180364.earntalktime.com').text("Earn Talktime (Very Good)");
+$('#5').attr('href','http://share.tbal.io/v2/app?m=1&code=2VCKZTJR').text("True Balance (Very Good)");
+$('#6').attr('href','https://42.slde.io/c9ee791b').text("Slide (Very Good)");
+$('#7').attr('href','https://mcent.com/app/?mcode=89FRCZ&tcx=OTHR').text("MCent (Very Good)");
+$('#8').attr('href','http://ladooo.com/304646008').text("Ladoo (Good)");
+$('#9').attr('href','http://installdataback.com/SID31285').text("Databack (Good)");
+$('#10').attr('href','http://tbk.bz/j7ynguyg').text("Task Bucks (OK)");
+})
