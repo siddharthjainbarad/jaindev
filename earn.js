0e5a09e18a9260dbb983c5852e116218439e2fe3
siddharthisenxt <siddharthisnext@gmail.com>
2017-01-09 13:15:20 +0530

    Update earn.js

--- earn.js
+++ earn.js
@@ -8,5 +8,5 @@ $('#6').attr('href','https://42.slde.io/c9ee791b').text("Slide (Very Good)");
 $('#7').attr('href','https://mcent.com/app/?mcode=89FRCZ&tcx=OTHR').text("MCent (Very Good)");
 $('#8').attr('href','http://ladooo.com/304646008').text("Ladoo (Good)");
 $('#9').attr('href','http://installdataback.com/SID31285').text("Databack (Good)");
-$('#10').attr('href','http://tbk.bz/j7ynguyg').text("Task Bucks (OK)");
+$('#10').attr('href','http://tbk.bz/j7ynguyg').text("Task Bucks (OKs)");
 })
