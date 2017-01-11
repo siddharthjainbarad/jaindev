4daf63d17884307667ba1dadb8131ffe0bc47d45
siddharthisenxt <siddharthisnext@gmail.com>
2017-01-11 12:21:19 +0530

    Update earn.js

--- earn.js
+++ earn.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-$('#1').attr('href','http://the-pocket-money.com/pm/edcb4e14db3446d4f2548b33c6a85f31').text("Pocket Money (Best)");
-$('#2').attr('href','http://www.freeb.in/FreeB/ws/invite/W_6ET30637').text("FreeB (Excellent)");
-$('#3').attr('href','https://www.fokat.co.in/fkt/ws/invite/F1DW7N635').text("Fokat Money (Excellent)");
-$('#4').attr('href','http://180364.earntalktime.com').text("Earn Talktime (Very Good)");
+$('#2').attr('href','http://the-pocket-money.com/pm/edcb4e14db3446d4f2548b33c6a85f31').text("Pocket Money (Best)");
+$('#3').attr('href','http://www.freeb.in/FreeB/ws/invite/W_6ET30637').text("FreeB (Excellent)");
+$('#4').attr('href','https://www.fokat.co.in/fkt/ws/invite/F1DW7N635').text("Fokat Money (Excellent)");
+$('#1').attr('href','http://180364.earntalktime.com').text("Earn Talktime (Best)");
 $('#5').attr('href','http://share.tbal.io/v2/app?m=1&code=2VCKZTJR').text("True Balance (Very Good)");
 $('#6').attr('href','https://42.slde.io/c9ee791b').text("Slide (Very Good)");
 $('#7').attr('href','https://mcent.com/app/?mcode=89FRCZ&tcx=OTHR').text("MCent (Very Good)");
