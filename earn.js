b06758b86c9df6a2e9b251c2b3937e8944b63878
siddharthisenxt <siddharthisnext@gmail.com>
2017-02-01 18:17:34 +0530

    Update earn.js

--- earn.js
+++ earn.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 $('#3').attr('href','http://the-pocket-money.com/pm/edcb4e14db3446d4f2548b33c6a85f31').text("Pocke Money (Excellent)");
 $('#2').attr('href','http://www.freeb.in/FreeB/ws/invite/W_6ET30637').text("FreeB (Best)");
 $('#4').attr('href','https://www.fokat.co.in/fkt/ws/invite/F1DW7N635').text("Fokat Money (Excellent)");
-$('#1').attr('href','http://180875.earntalktime.com').text("Earn Talktime (Best)");
+$('#1').attr('href','http://26503900.earntalktime.com').text("Earn Talktime (Best)");
 $('#5').attr('href','http://share.tbal.io/v2/app?m=1&code=2YUGF36A').text("True Balance (Very Good)");
 $('#6').attr('href','https://42.slde.io/c9ee791b').text("Slide (Very Good)");
 $('#7').attr('href','https://mcent.com/app/?mcode=89FRCZ&tcx=OTHR').text("MCent (Very Good)");
