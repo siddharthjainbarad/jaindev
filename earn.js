7b455402d2cc8736b91584cb97b8d2bcb0ac3415
siddharthisenxt <siddharthisnext@gmail.com>
2017-02-07 15:19:42 +0530

    Update earn.js

--- earn.js
+++ earn.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-$('#3').attr('href','http://the-pocket-money.com/pm/edcb4e14db3446d4f2548b33c6a85f31').text("Pocket Money (Excellent)");
+$('#4').attr('href','http://the-pocket-money.com/pm/edcb4e14db3446d4f2548b33c6a85f31').text("Pocket Money (Excellent)");
 $('#2').attr('href','http://www.freeb.in/FreeB/ws/invite/W_6ET30637').text("FreeB (Best)");
-$('#4').attr('href','https://www.fokat.co.in/fkt/ws/invite/F1DW7N635').text("Fokat Money (Excellent)");
+$('#5').attr('href','https://www.fokat.co.in/fkt/ws/invite/F1DW7N635').text("Fokat Money (Excellent)");
 $('#1').attr('href','http://180364.earntalktime.com').text("Earn Talktime (Best)");
-$('#5').attr('href','http://share.tbal.io/v2/app?m=1&code=2YUGF36A').text("True Balance (Very Good)");
-$('#6').attr('href','https://42.slde.io/c9ee791b').text("Slide (Very Good)");
-$('#7').attr('href','https://mcent.com/app/?mcode=89FRCZ&tcx=OTHR').text("MCent (Very Good)");
-$('#8').attr('href','http://ladooo.com/304646008').text("Ladoo (Good)");
-$('#9').attr('href','http://installdataback.com/SID31285').text("Databack (Good)");
+$('#6').attr('href','http://share.tbal.io/v2/app?m=1&code=2YUGF36A').text("True Balance (Very Good)");
+$('#7').attr('href','https://42.slde.io/c9ee791b').text("Slide (Very Good)");
+$('#8').attr('href','https://mcent.com/app/?mcode=89FRCZ&tcx=OTHR').text("MCent (Very Good)");
+$('#9').attr('href','http://ladooo.com/304646008').text("Ladoo (Good)");
+$('#3').attr('href','http://mangorecharge.com/a/r.php?r=NTg5OTk2Yzg0ZDgyZA').text("MANGO (Excellent)");
 $('#10').attr('href','http://tbk.bz/j7ynguyg').text("Task Bucks (OK)");
 })
