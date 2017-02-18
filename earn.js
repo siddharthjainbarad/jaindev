d96e1b16336437b2f87ace42071f5aa8f6b4f6da
siddharthisenxt <siddharthisnext@gmail.com>
2017-02-18 15:43:10 +0530

    Update earn.js

--- earn.js
+++ earn.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
-$('#4').attr('href','http://the-pocket-money.com/pm/edcb4e14db3446d4f2548b33c6a85f31').text("Pocket Money (Excellent)");
-$('#2').attr('href','http://www.freeb.in/FreeB/ws/invite/W_6ET30637').text("FreeB (Best)");
-$('#5').attr('href','https://www.fokat.co.in/fkt/ws/invite/F1DW7N635').text("Fokat Money (Excellent)");
-$('#3').attr('href','http://180364.earntalktime.com').text("Earn Talktime (Best)");
+$('#1').attr('href','http://www.freeb.in/FreeB/ws/invite/W_6ET30637').text("FreeB (Best)");
+$('#2').attr('href','http://180364.earntalktime.com').text("Earn Talktime (Best)");
+$('#3').attr('href','http://the-pocket-money.com/pm/edcb4e14db3446d4f2548b33c6a85f31').text("Pocket Money (Excellent)");
+$('#4').attr('href','https://www.fokat.co.in/fkt/ws/invite/F1DW7N635').text("Fokat Money (Excellent)");
+$('#5').attr('href','https://cashboss.in/invite?refcode=3BC487&source=referral').text("Cash Boss (Very Good)");
+$('#6').attr('href','http://luckycashfree.com/earncash_main_service/re?funid=2010&code=6JB3YnzC_new').text("LuckyCash (Very Good)");
 $('#7').attr('href','http://share.tbal.io/v2/app?m=1&code=2YUGF36A').text("True Balance (Very Good)");
 $('#8').attr('href','https://42.slde.io/c9ee791b').text("Slide (Very Good)");
 $('#9').attr('href','https://mcent.com/app/?mcode=89FRCZ&tcx=OTHR').text("MCent (Very Good)");
-$('#6').attr('href','http://luckycashfree.com/earncash_main_service/re?funid=2010&code=6JB3YnzC_new').text("LuckyCash (Very Good)");
-$('#1').attr('href','http://champcash.com/9589745').text("ChampCash 9589745 (Best)");
-$('#10').attr('href','http://tbk.bz/j7ynguyg').text("Task Bucks (OK)");
+$('#10').attr('href','http://champcash.com/9589745').text("ChampCash 9589745 (Very Good)");
+
 })
