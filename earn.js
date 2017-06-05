1fff87fd193082469eaa9d8d02131b152b091905
siddharthisenxt <siddharthisnext@gmail.com>
2017-06-05 10:36:10 -0700

    Update earn.js

--- earn.js
+++ earn.js
@@ -6,7 +6,7 @@ $('#4').attr('href','http://the-pocket-money.com/pm/edcb4e14db3446d4f2548b33c6a8
 $('#3').attr('href','https://www.fokat.co.in/fkt/ws/invite/F1DW7N635').text("Fokat Money (Excellent)");
 $('#6').attr('href','https://cashboss.in/invite?refcode=3BC487&source=referral').text("Cash Boss (Very Good)");
 $('#5').attr('href','http://m.d11.io/Dream11App').text("DREAM11 (LEGAL BETTING)(Rs 100 CODE- SIDDH3372YZ)");
-$('#7').attr('href','http://share.tbal.io/v2/app?m=1&code=2YUGF36A').text("True Balance (Very Good)");
+$('#7').attr('href','https://www.uber.com/invite/siddharthj3830ue').text("Free UBER Rides (Very Good)");
 $('#8').attr('href','https://42.slde.io/c9ee791b').text("Slide (Very Good)");
 $('#9').attr('href','https://mcent.com/app/?mcode=89FRCZ&tcx=OTHR').text("MCent (Very Good)");
 $('#10').attr('href','http://champcash.com/9589745').text("ChampCash 9589745 (Very Good)");
