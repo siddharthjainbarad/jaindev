7322bdf10916ffa44c7c008705da9edd899b8e16
siddharthisenxt <siddharthisnext@gmail.com>
2017-07-29 01:06:26 -0700

    Update onlinne.js

--- onlinne.js
+++ onlinne.js
@@ -1,107 +1,107 @@
 $(document).ready(function() {
-$('#1-1').attr('href','http://csl.ink/3mWC').text("1. ClixSense");
-$('#1-2').attr('href','https://www.neobux.com/').text("2. NeoBux");
-$('#1-3').attr('href','http://buxp.org/').text("3. BuxP");
-$('#1-4').attr('href','https://www.paidverts.com/').text("4. Paidverts");
+$('#11').attr('href','http://csl.ink/3mWC').text("1. ClixSense");
+$('#12').attr('href','https://www.neobux.com/').text("2. NeoBux");
+$('#13').attr('href','http://buxp.org/').text("3. BuxP");
+$('#14').attr('href','https://www.paidverts.com/').text("4. Paidverts");
 
-$('#2-1').attr('href','https://www.inboxdollars.com/').text("1. InboxDollars");
-$('#2-2').attr('href','https://www.cashcrate.com/').text("2. CashCrate");
-$('#2-3').attr('href','https://www.swagbucks.com/').text("3. Swagbucks");
-$('#2-4').attr('href','http://csl.ink/3mWC').text("4. Clixsense");
-$('#2-5').attr('href','https://www.treasuretrooper.com/').text("5. TreasureTrooperx");
+$('#21').attr('href','https://www.inboxdollars.com/').text("1. InboxDollars");
+$('#22').attr('href','https://www.cashcrate.com/').text("2. CashCrate");
+$('#23').attr('href','https://www.swagbucks.com/').text("3. Swagbucks");
+$('#24').attr('href','http://csl.ink/3mWC').text("4. Clixsense");
+$('#25').attr('href','https://www.treasuretrooper.com/').text("5. TreasureTrooperx");
 
-$('#3-1').attr('href','https://kolotibablo.com/').text("1. Kolotibablo");
-$('#3-2').attr('href','http://www.megatypers.com/').text("2. MegaTypers (Invite Code - DBP2)");
-$('#3-3').attr('href','http://captchatypers.com/Forms/login.aspx').text("3. CaptchaTypers");
-$('#3-4').attr('href','http://www.protypers.com/register').text("4. ProTypers");
-$('#3-5').attr('href','http://captcha2cash.com/').text("5. Captcha2Cash");
-$('#3-6').attr('href','https://2captcha.com/').text("6. 2Captcha");
-$('#3-7').attr('href','http://www.qlinkgroup.com/op/login').text("7. Qlinkgroup");
-$('#3-8').attr('href','https://thesmartcrowd.lionbridge.com/').text("8. VirtualBee");
-$('#3-9').attr('href','http://support.fasttypers.org/Main/frmRegister.aspx').text("9. FastTypers");
-$('#3-10').attr('href','http://free-dataentry.com/pixprofit/').text("10. PixProfit");
+$('#31').attr('href','https://kolotibablo.com/').text("1. Kolotibablo");
+$('#32').attr('href','http://www.megatypers.com/').text("2. MegaTypers (Invite Code - DBP2)");
+$('#33').attr('href','http://captchatypers.com/Forms/login.aspx').text("3. CaptchaTypers");
+$('#34').attr('href','http://www.protypers.com/register').text("4. ProTypers");
+$('#35').attr('href','http://captcha2cash.com/').text("5. Captcha2Cash");
+$('#36').attr('href','https://2captcha.com/').text("6. 2Captcha");
+$('#37').attr('href','http://www.qlinkgroup.com/op/login').text("7. Qlinkgroup");
+$('#38').attr('href','https://thesmartcrowd.lionbridge.com/').text("8. VirtualBee");
+$('#39').attr('href','http://support.fasttypers.org/Main/frmRegister.aspx').text("9. FastTypers");
+$('#310').attr('href','http://free-dataentry.com/pixprofit/').text("10. PixProfit");
 
-$('#4-1').attr('href','https://www.inboxdollars.com/').text("1. InboxDollars");
-$('#4-2').attr('href','http://www.tellypulse.com/').text("2. Star Panel");
-$('#4-3').attr('href','https://in.ipanelonline.com/').text("3. iPanelOnline India");
-$('#4-4').attr('href','https://www.ipoll.com/').text("4. SurveyHead");
-$('#4-5').attr('href','https://in.toluna.com/').text("5. Toluna India");
-$('#4-6').attr('href','http://www.indiaspeaks.net/').text("6. IndiaSpeaks");
-$('#4-7').attr('href','https://www.brandinstitute.com/memberservices/default.aspx?task=register').text("7. Brand Institute");
-$('#4-8').attr('href','https://www.permissionresearch.com/').text("8. PermissionResearch");
-$('#4-9').attr('href','https://in.toluna.com/').text("9. Toluna");
-$('#4-10').attr('href','https://www.surveysavvy.com/').text("10. SurveySavvy");
-$('#4-11').attr('href','https://spidermetrix.com/').text("11. Spider Metrix");
-$('#4-12').attr('href','https://www.globaltestmarket.com/?lang=E').text("12. GlobalTestMarket");
-$('#4-13').attr('href','https://www.valuedopinions.co.in/').text("13. ValuedOpinions India");
-$('#4-14').attr('href','http://thepanelstation.com/in/home').text("14. The Panel Station");
-$('#4-15').attr('href','http://www.yoursay.com/').text("15. YourSay");
-$('#4-16').attr('href','http://www.vipvoice.com/').text("16. VIP Voice");
-$('#4-17').attr('href','http://www.panelplace.com/').text("17. PanelPlace");
-$('#4-18').attr('href','https://www.harrispollonline.com/').text("18. The Harris Poll Online (HPOL)");
-$('#4-19').attr('href','https://socratic.org/').text("19. Socratic Forum");
-$('#4-20').attr('href','http://www.planetpanel.net/').text("20. PlanetPanel");
+$('#41').attr('href','https://www.inboxdollars.com/').text("1. InboxDollars");
+$('#42').attr('href','http://www.tellypulse.com/').text("2. Star Panel");
+$('#43').attr('href','https://in.ipanelonline.com/').text("3. iPanelOnline India");
+$('#44').attr('href','https://www.ipoll.com/').text("4. SurveyHead");
+$('#45').attr('href','https://in.toluna.com/').text("5. Toluna India");
+$('#46').attr('href','http://www.indiaspeaks.net/').text("6. IndiaSpeaks");
+$('#47').attr('href','https://www.brandinstitute.com/memberservices/default.aspx?task=register').text("7. Brand Institute");
+$('#48').attr('href','https://www.permissionresearch.com/').text("8. PermissionResearch");
+$('#49').attr('href','https://in.toluna.com/').text("9. Toluna");
+$('#410').attr('href','https://www.surveysavvy.com/').text("10. SurveySavvy");
+$('#411').attr('href','https://spidermetrix.com/').text("11. Spider Metrix");
+$('#412').attr('href','https://www.globaltestmarket.com/?lang=E').text("12. GlobalTestMarket");
+$('#413').attr('href','https://www.valuedopinions.co.in/').text("13. ValuedOpinions India");
+$('#414').attr('href','http://thepanelstation.com/in/home').text("14. The Panel Station");
+$('#415').attr('href','http://www.yoursay.com/').text("15. YourSay");
+$('#416').attr('href','http://www.vipvoice.com/').text("16. VIP Voice");
+$('#417').attr('href','http://www.panelplace.com/').text("17. PanelPlace");
+$('#418').attr('href','https://www.harrispollonline.com/').text("18. The Harris Poll Online (HPOL)");
+$('#419').attr('href','https://socratic.org/').text("19. Socratic Forum");
+$('#420').attr('href','http://www.planetpanel.net/').text("20. PlanetPanel");
 
-$('#5-1').attr('href','https://affiliate-program.amazon.in/').text("1. Amazon Affiliate");
-$('#5-2').attr('href','https://affiliate.flipkart.com/login').text("2. Flipkart Affiliate");
-$('#5-3').attr('href','https://affiliate.snapdeal.com/').text("3. Snapdeal Affiliate");
-$('#5-4').attr('href','http://www.clickbank.com/affiliate-network/').text("4. ClickBank Affiliate");
-$('#5-5').attr('href','http://www.cj.com/').text("5. CJ Affiliate");
+$('#51').attr('href','https://affiliate-program.amazon.in/').text("1. Amazon Affiliate");
+$('#52').attr('href','https://affiliate.flipkart.com/login').text("2. Flipkart Affiliate");
+$('#53').attr('href','https://affiliate.snapdeal.com/').text("3. Snapdeal Affiliate");
+$('#54').attr('href','http://www.clickbank.com/affiliate-network/').text("4. ClickBank Affiliate");
+$('#55').attr('href','http://www.cj.com/').text("5. CJ Affiliate");
 
-$('#6-1').attr('href','https://www.elance.com/login').text("1. Elance");
-$('#6-2').attr('href','https://www.freelancer.in/').text("2. Freelancer");
-$('#6-3').attr('href','http://worknhire.com/').text("3. WorkNHire");
-$('#6-4').attr('href','https://www.upwork.com/').text("4. Upwork");
-$('#6-5').attr('href','https://www.peopleperhour.com/').text("5. People Per Hour");
+$('#61').attr('href','https://www.elance.com/login').text("1. Elance");
+$('#62').attr('href','https://www.freelancer.in/').text("2. Freelancer");
+$('#63').attr('href','http://worknhire.com/').text("3. WorkNHire");
+$('#64').attr('href','https://www.upwork.com/').text("4. Upwork");
+$('#65').attr('href','https://www.peopleperhour.com/').text("5. People Per Hour");
 
-$('#7-1').attr('href','https://www.hiremymom.com/').text("1. HireMyMom");
-$('#7-2').attr('href','https://mytasker.com/').text("2. MyTasker");
-$('#7-3').attr('href','https://www.zirtual.com/').text("3. Zirtual");
-$('#7-4').attr('href','https://uassistme.co/').text("4. uAssistMe");
-$('#7-5').attr('href','http://www.123employee.com/').text("5. 123Employee");
+$('#71').attr('href','https://www.hiremymom.com/').text("1. HireMyMom");
+$('#72').attr('href','https://mytasker.com/').text("2. MyTasker");
+$('#73').attr('href','https://www.zirtual.com/').text("3. Zirtual");
+$('#74').attr('href','https://uassistme.co/').text("4. uAssistMe");
+$('#75').attr('href','http://www.123employee.com/').text("5. 123Employee");
 
-$('#8-1').attr('href','https://www.elance.com/login').text("1. Elance");
-$('#8-2').attr('href','https://www.iwriter.com/').text("2. iWriter");
-$('#8-3').attr('href','http://india.writerbay.com/').text("3. WriterBay");
-$('#8-4').attr('href','https://www.freelancewriting.com/').text("4. FreelanceWriting");
-$('#8-5').attr('href','https://www.textbroker.com/').text("5. TextBroker");
-$('#8-6').attr('href','https://expresswriters.com/').text("6. ExpressWriters");
-$('#8-7').attr('href','http://www.freelancewritinggigs.com/').text("7. FreelanceWritingGigs");
+$('#81').attr('href','https://www.elance.com/login').text("1. Elance");
+$('#82').attr('href','https://www.iwriter.com/').text("2. iWriter");
+$('#83').attr('href','http://india.writerbay.com/').text("3. WriterBay");
+$('#84').attr('href','https://www.freelancewriting.com/').text("4. FreelanceWriting");
+$('#85').attr('href','https://www.textbroker.com/').text("5. TextBroker");
+$('#86').attr('href','https://expresswriters.com/').text("6. ExpressWriters");
+$('#87').attr('href','http://www.freelancewritinggigs.com/').text("7. FreelanceWritingGigs");
 
-$('#9-1').attr('href','http://seotrainingcourse.co.in/').text("1. SEOTrainingCourse");
+$('#91').attr('href','http://seotrainingcourse.co.in/').text("1. SEOTrainingCourse");
 
-$('#10-1').attr('href','https://www.fiverr.com/').text("1. Fiverr");
+$('#101').attr('href','https://www.fiverr.com/').text("1. Fiverr");
 
-$('#11-1').attr('href','https://www.mturk.com/').text("1. Amazon mTurk");
-$('#11-2').attr('href','https://microworkers.com/').text("2. MicroWorker");
-$('#11-3').attr('href','https://www.seoclerk.com/').text("3. SEOClerk");
-$('#11-4').attr('href','https://www.clickworker.com/').text("4. ClickWorker");
-$('#11-5').attr('href','http://www.gigwalk.com/').text("5. GigWalk");
+$('#111').attr('href','https://www.mturk.com/').text("1. Amazon mTurk");
+$('#112').attr('href','https://microworkers.com/').text("2. MicroWorker");
+$('#113').attr('href','https://www.seoclerk.com/').text("3. SEOClerk");
+$('#114').attr('href','https://www.clickworker.com/').text("4. ClickWorker");
+$('#115').attr('href','http://www.gigwalk.com/').text("5. GigWalk");
 
-$('#12-1').attr('href','https://www.youtube.com/create_channel').text("1. Create Youtube Channel");
+$('#121').attr('href','https://www.youtube.com/create_channel').text("1. Create Youtube Channel");
 
-$('#13-1').attr('href','https://seller.flipkart.com/').text("1. Flipkart Seller");
-$('#13-2').attr('href','https://sellercentral.amazon.in/').text("2. Amazon Seller");
-$('#13-3').attr('href','https://seller.paytm.com/').text("3. PayTM Seller");
-$('#13-4').attr('href','https://sellers.snapdeal.com/').text("4. Snapdeal Seller");
+$('#131').attr('href','https://seller.flipkart.com/').text("1. Flipkart Seller");
+$('#132').attr('href','https://sellercentral.amazon.in/').text("2. Amazon Seller");
+$('#133').attr('href','https://seller.paytm.com/').text("3. PayTM Seller");
+$('#134').attr('href','https://sellers.snapdeal.com/').text("4. Snapdeal Seller");
 
-$('#14-1').attr('href','http://bluehost.in/?utm_source=affiliate&a_aid=b71b86fb').text("1. BlueHost");
+$('#141').attr('href','http://bluehost.in/?utm_source=affiliate&a_aid=b71b86fb').text("1. BlueHost");
 
-$('#17-1').attr('href','http://180364.earntalktime.com').text("1. Earn Talktime");
-$('#17-2').attr('href','http://www.freeb.in/FreeB/ws/invite/W_6ET30637').text("2. FreeB");
-$('#17-3').attr('href','https://www.fokat.co.in/fkt/ws/invite/F1DW7N635').text("3. Fokat Money");
-$('#17-4').attr('href','http://the-pocket-money.com/pm/af0aac5332c226d748a1d5306bb7f288').text("4. Poket Money");
-$('#17-5').attr('href','https://cashboss.in/invite?refcode=3BC487&source=referral').text("5. CashBoss");
-$('#17-6').attr('href','https://42.slde.io/c9ee791b').text("6. Slide");
-$('#17-7').attr('href','http://m.d11.io/Dream11App').text("7. Dream11 Free Rs100 (SIDDH3372YZ)");
-$('#17-8').attr('href','http://champcash.com/9589745').text("8. ChampCash (9589745)");
+$('#171').attr('href','http://180364.earntalktime.com').text("1. Earn Talktime");
+$('#172').attr('href','http://www.freeb.in/FreeB/ws/invite/W_6ET30637').text("2. FreeB");
+$('#173').attr('href','https://www.fokat.co.in/fkt/ws/invite/F1DW7N635').text("3. Fokat Money");
+$('#174').attr('href','http://the-pocket-money.com/pm/af0aac5332c226d748a1d5306bb7f288').text("4. Poket Money");
+$('#175').attr('href','https://cashboss.in/invite?refcode=3BC487&source=referral').text("5. CashBoss");
+$('#176').attr('href','https://42.slde.io/c9ee791b').text("6. Slide");
+$('#177').attr('href','http://m.d11.io/Dream11App').text("7. Dream11 Free Rs100 (SIDDH3372YZ)");
+$('#178').attr('href','http://champcash.com/9589745').text("8. ChampCash (9589745)");
 
-$('#18-1').attr('href','https://www.shutterstock.com/').text("1. Shutterstock");
-$('#18-2').attr('href','https://www.fotolia.com/').text("2. Fotolia");
-$('#18-3').attr('href','http://www.istockphoto.com/in').text("3. iStockPhoto");
-$('#18-4').attr('href','http://photobucket.com/').text("4. Photobucket");
+$('#181').attr('href','https://www.shutterstock.com/').text("1. Shutterstock");
+$('#182').attr('href','https://www.fotolia.com/').text("2. Fotolia");
+$('#183').attr('href','http://www.istockphoto.com/in').text("3. iStockPhoto");
+$('#184').attr('href','http://photobucket.com/').text("4. Photobucket");
 
-$('#19-1').attr('href','https://www.olx.in/').text("1. OLX");
-$('#19-2').attr('href','http://www.quikr.com/').text("2. Quikr");
+$('#191').attr('href','https://www.olx.in/').text("1. OLX");
+$('#192').attr('href','http://www.quikr.com/').text("2. Quikr");
 
 })
