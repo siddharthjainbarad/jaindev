cb1f037a9ecadb40bf5ed32dd577d86fbbfc180e
siddharthisenxt <siddharthisnext@gmail.com>
2017-08-08 20:47:49 +0530

    Update earn.js

--- earn.js
+++ earn.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-$('#2').attr('href','http://180364.earntalktime.com').text("Earn Talktime (Best)");
+$('#3').attr('href','http://180364.earntalktime.com').text("Earn Talktime LINK 3(Best)");
+  $('#2').attr('href','http://180364.earntalktime.com').text("Earn Talktime LINK 2(Best)");
 $('#1').attr('href','http://26503900.earntalktime.com').text("Earn Talktime (Best)");
 //$('#4').attr('href','http://the-pocket-money.com/pm/edcb4e14db3446d4f2548b33c6a85f31').text("Pocket Money (Excellent)");
 $('#4').attr('href','http://the-pocket-money.com/pm/af0aac5332c226d748a1d5306bb7f288').text("Pocket Money (Excellent)");
-$('#3').attr('href','https://www.fokat.co.in/fkt/ws/invite/F1DW7N635').text("Fokat Money (Excellent)");
 $('#6').attr('href','https://cashboss.in/invite?refcode=3BC487&source=referral').text("Cash Boss (Very Good)");
 $('#5').attr('href','http://m.d11.io/Dream11App').text("DREAM11 (LEGAL BETTING)(Rs 100 CODE- SIDDH3372YZ)");
 $('#7').attr('href','https://www.uber.com/invite/siddharthj3830ue').text("Free UBER Rides (Very Good)");
