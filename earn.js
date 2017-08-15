3aa0e52cfb751c57c653c3ddade9ee341f6fd127
siddharthisenxt <siddharthisnext@gmail.com>
2017-08-15 02:51:15 -0700

    Update earn.js

--- earn.js
+++ earn.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-$('#3').attr('href','http://27162991.earntalktime.com').text("Earn Talktime LINK 3(Best)");
-  $('#2').attr('href','http://180364.earntalktime.com').text("Earn Talktime LINK 2(Best)");
-$('#1').attr('href','http://26503900.earntalktime.com').text("Earn Talktime (Best)");
+$('#3').attr('href','http://27162991.earntalktime.com').text("Earn Talktime LINK 2(Best)");
+$('#2').attr('href','http://180364.earntalktime.com').text("Earn Talktime(Best)");
+$('#1').attr('href','https://phon.pe/ru_siddl4rm').text("Use and Get FLAT Rs 100 PhonePe (Best)");
+//$('#1').attr('href','http://26503900.earntalktime.com').text("Earn Talktime (Best)");
 //$('#4').attr('href','http://the-pocket-money.com/pm/edcb4e14db3446d4f2548b33c6a85f31').text("Pocket Money (Excellent)");
 $('#4').attr('href','http://the-pocket-money.com/pm/af0aac5332c226d748a1d5306bb7f288').text("Pocket Money (Excellent)");
 $('#6').attr('href','https://cashboss.in/invite?refcode=3BC487&source=referral').text("Cash Boss (Very Good)");
@@ -10,5 +11,4 @@ $('#7').attr('href','https://www.uber.com/invite/siddharthj3830ue').text("Free U
 $('#8').attr('href','https://42.slde.io/c9ee791b').text("Slide (Very Good)");
 $('#9').attr('href','https://mcent.com/app/?mcode=89FRCZ&tcx=OTHR').text("MCent (Very Good)");
 $('#10').attr('href','http://champcash.com/9589745').text("ChampCash 9589745 (Very Good)");
-https://phon.pe/ru_siddl4rm
 })
