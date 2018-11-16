9cf1bc8ff1498552b2d08168dc8f28ff86887dc9
siddharthisenxt <siddharthisnext@gmail.com>
2018-11-16 11:37:47 +0530

    Update earn.js

--- earn.js
+++ earn.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 $('#8').attr('href','http://27162991.earntalktime.com').text("Earn Talktime LINK 2(Best)");
 //$('#2').attr('href','http://180364.earntalktime.com').text("Earn Talktime(Best)");
-$('#4').attr('href','https://phon.pe/ru_siddl4rm').text("Use and Get FLAT Rs 100 PhonePe (Best)");
-$('#2').attr('href','http://26503900.earntalktime.com').text("Earn Talktime (Best)");
+$('#2').attr('href','https://phon.pe/ru_siddl4rm').text("Use and Get FLAT Rs 100 PhonePe (Best)");
+$('#9').attr('href','http://26503900.earntalktime.com').text("Earn Talktime (Best)");
 //$('#4').attr('href','http://the-pocket-money.com/pm/edcb4e14db3446d4f2548b33c6a85f31').text("Pocket Money (Excellent)");
-$('#1').attr('href','http://the-pocket-money.com/pm/af0aac5332c226d748a1d5306bb7f288').text("Pocket Money (Excellent)");
+$('#3').attr('href','http://the-pocket-money.com/pm/af0aac5332c226d748a1d5306bb7f288').text("Pocket Money (Excellent)");
 $('#6').attr('href','https://cashboss.in/invite?refcode=3BC487&source=referral').text("Cash Boss (Very Good)");
 $('#5').attr('href','http://m.d11.io/Dream11App').text("DREAM11 (LEGAL BETTING)(Rs 100 CODE- SIDDH3372YZ)");
-$('#3').attr('href','https://g.co/tez/193g1').text("Tez from Google Rs.51 Bonus(Best)");
-$('#7').attr('href','https://42.slde.io/c9ee791b').text("Slide (Very Good)");
-$('#9').attr('href','https://mcent.com/app/?mcode=89FRCZ&tcx=OTHR').text("MCent (Very Good)");
-$('#10').attr('href','http://champcash.com/9589745').text("ChampCash 9589745 (Very Good)");
+$('#1').attr('href','https://g.co/tez/193g1').text("Tez from Google Rs.201 Bonus(Best)");
+$('#7').attr('href','http://r.bigbasket.com/bigb2adfo-1nq/3').text("Flat Rs100 on Signup (Very Good)");
+$('#4').attr('href','https://browser.mcent.com/r/7A8ZAB4CRW6-u4V9GYRbeg').text("MCent (Very Good)");
+$('#10').attr('href',' https://dunzo.onelink.me/jWqC?referralCode=SGUJFO').text("Order and Get Flat Rs150 (Very Good)");
 })
